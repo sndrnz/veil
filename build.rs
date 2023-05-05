d4a1948bb7471f892ba503977a3547f80617cf4a
Sandro Darnuzer <sandro.darnuzer@gmail.com>
2023-05-05 10:39:16 +0200

    improve build script

--- build.rs
+++ build.rs
@@ -1,21 +1,46 @@
+use clap::Command;
 use clap::CommandFactory;
 use clap_complete::generate_to;
-use clap_complete::shells;
+use clap_complete::Shell;
 use std::env;
-use std::io::Error;
+use std::error::Error;
 
 include!("src/cli.rs");
 
-fn main() -> Result<(), Error> {
-    let outdir = match env::var_os("OUT_DIR") {
-        None => return Ok(()),
-        Some(outdir) => outdir,
-    };
+fn generate_completion(
+    mut cmd: &mut Command,
+    shell: Shell,
+    bin_name: &str,
+    out_dir: &PathBuf,
+) -> Result<(), Box<dyn Error>> {
+    let path = generate_to(shell, &mut cmd, bin_name, out_dir)?;
+    println!("completion file for '{:?}' is generated: {:?}", shell, path);
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("cargo:rerun-if-changed=cli.rs");
+
+    let cwd = env::current_dir()?;
+    let out_dir = cwd.join("completions");
+
+    if !out_dir.exists() {
+        std::fs::create_dir(&out_dir)?;
+    }
 
     let mut cmd = Cli::command();
-    let path = generate_to(shells::Zsh, &mut cmd, "veil", outdir)?;
 
-    println!("cargo:warning=completion file is generated: {:?}", path);
+    let shells = [
+        Shell::Zsh,
+        Shell::Bash,
+        Shell::Elvish,
+        Shell::Fish,
+        Shell::PowerShell,
+    ];
+
+    for shell in shells.iter() {
+        generate_completion(&mut cmd, *shell, "veil", &out_dir)?;
+    }
 
     Ok(())
 }
